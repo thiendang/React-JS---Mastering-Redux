109dd54d80bf546c7f00161936f346b12a95ad17
ThienDang <danghoangthien.hcmus@gmail.com>
2017-07-14 09:20:04 +0700

    section5-lecture22

--- src/app.js
+++ src/app.js
@@ -2,13 +2,16 @@ import React , {Component} from 'react';
 import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
-import characters from './reducers';
+import rootReducers from './reducers';
 
 import App from './components/App.js';
 import css from './app.scss';
+import {addCharacterById} from './actions';
 
-const store = createStore(characters);
+const store = createStore(rootReducers);
 console.log(store.getState());
+store.subscribe(() => console.log('store ', store.getState()));
+store.dispatch(addCharacterById(2));
 
 ReactDOM.render(
   <Provider store={store}>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,11 @@
 import characters_json from '../data/characters.json';
+import {ADD_CHARACTER} from '../actions/index'
 
 function characters(state = characters_json, action) {
   switch (action.type) {
-
+    case ADD_CHARACTER:
+      let characters = state.filter(item => item.id !== action.id);
+      return characters;
     default:
       return state;
   }
