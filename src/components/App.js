0e5b9152cf7718c45333c94377a2cdba4bfe29df
ThienDang <danghoangthien.hcmus@gmail.com>
2017-07-15 14:07:05 +0700

    section5-lecture29

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import CharacterList from './CharacterList';
 import HeroList from './HeroList';
+import SquadStats from './SquadStats';
 
 export default class App extends Component {
   constructor(props) {
@@ -13,11 +14,14 @@ export default class App extends Component {
     return(
       <div className="App">
         <h2>SuperSquad</h2>
-        <div className="col-md-6">
+        <div className="col-md-4">
           <CharacterList/>
         </div>
-        <div className="col-md-6">
+        <div className="col-md-4">
           <HeroList/>
+        </div>
+        <div className="col-md-4">
+          <SquadStats/>
         </div>
       </div>
     )
