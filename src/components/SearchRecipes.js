0e64631ab4fe7b254315bd1b49296e79d8babb4e
ThienDang <danghoangthien.hcmus@gmail.com>
2017-07-17 14:28:14 +0700

    section6-lecture35

--- src/components/SearchRecipes.js
+++ src/components/SearchRecipes.js
@@ -2,21 +2,34 @@ import React, {Component} from 'react';
 import {Form, FormGroup, FormControl, ControlLabel, Button} from 'react-bootstrap';
 
 class SearchRecipes extends Component {
+  constructor(){
+    super();
+    this.state = {
+      ingredients: '',
+      dish: ''
+    }
+  }
+  search = () => {
+    let {ingredients, dish} = this.state;
+    const url = `http://www.recipepuppy.com/api/?=${ingredients}&q=${dish}`;
+    console.log('state', this.state, 'url', url);
+  }
   render(){
     return(
       <Form inline>
         <FormGroup>
           <ControlLabel>Ingredients</ControlLabel>
           {' '}
-          <FormControl type="text" placeholder="garlic, chicken"/>
+          <FormControl onChange={event => this.setState({ingredients: event.target.value})} type="text" placeholder="garlic, chicken"/>
         </FormGroup>
         {' '}
         <FormGroup>
           <ControlLabel>Dish</ControlLabel>
           {' '}
-          <FormControl type="text" placeholder="adobo"/>
+          <FormControl onChange={event => this.setState({dish: event.target.value})} type="text" placeholder="adobo"/>
         </FormGroup>
-        <Button>Submit</Button>
+        {' '}
+        <Button onClick={this.search}>Submit</Button>
       </Form>
     )
   }
